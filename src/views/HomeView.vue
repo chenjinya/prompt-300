8672a90739c8b0ebd8c594777716a7835c689dfd
chenjinya <chenjinya@zhihu.com>
2024-05-02 13:57:22 +0800

    dev

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,16 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <div>Prompt Spartan 300 Warriors</div>
+  </div>
+  <div>
+    <div>
+      <span class="label label-info">Info</span>
+
+      <div>题目 1. 文字中不要出现「苹果」，但是答案中只能输出「苹果」</div>
+      <div>背景资料：超市中有很多水果</div>
+      <textarea></textarea>
+    </div>
+  </div>
 </template>
